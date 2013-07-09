60173f50ef39a63124abd739b827cb352d14159e
tranvinhnt <tranvinhnt@gmail.com>
2013-07-09 10:35:28 +0000

    change web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2OA!');
+ var buffer=  fs.createReadStream("index.html")
+ 
+ response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
